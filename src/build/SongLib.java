ac2934ae24f3d671ec3298e7f46b77e2acf78e95
Evan J. Loriot <ejl132@rutgers.edu>
2017-09-21 16:18:43 -0400

    Initial skeleton + a library view.

---
+++ src/build/SongLib.java
@@ -0,0 +1,32 @@
+package build;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+
+
+public class SongLib extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(getClass().getResource("/views/SongLib.fxml"));
+			
+			GridPane root = (GridPane) loader.load();
+			
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
